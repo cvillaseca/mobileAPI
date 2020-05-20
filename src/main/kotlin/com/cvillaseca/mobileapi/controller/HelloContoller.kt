c8e94e6b81c7d6234ed0a4fb1d8ced5e01690f6c
Cristian Villaseca <cristian@turo.com>
2020-05-19 23:16:37 -0700

    Update HelloController

--- src/main/kotlin/com/cvillaseca/mobileapi/controller/HelloContoller.kt
+++ src/main/kotlin/com/cvillaseca/mobileapi/controller/HelloContoller.kt
@@ -1,14 +1,36 @@
 package com.cvillaseca.mobileapi.controller
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
 @RestController
 @RequestMapping("/mobileApi")
-class HelloContoller {
+class HelloController {
     @RequestMapping(value = ["/helloWorld"], method = [(RequestMethod.GET)])
     fun getHelloWordMessage(): ResponseEntity<String> =
-            ResponseEntity.ok("Hello World")
+        ResponseEntity.ok("Hello World")
+
+    @RequestMapping(value = ["/helloWorld/{name}"], method = [(RequestMethod.GET)])
+    fun getHelloWordMessageWithName(
+        @PathVariable("name") name: String
+    ): ResponseEntity<Any> =
+        if (name != "Cristian") {
+            ResponseEntity.ok(
+                HelloResponse(
+                    message = "Hello $name",
+                    name = name
+                )
+            )
+        } else {
+            ResponseEntity.badRequest().body("I am Cristian")
+        }
 }
+
+data class HelloResponse(
+    val message: String,
+    val name: String
+)
